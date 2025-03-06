946ceba88585d538dfda0ad70b016c886e399a91
yogiefani <yogiefaniyc@gmail.com>
2025-03-07 04:18:21 +0700

    fix: message models to controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,5 +1,6 @@
 import * as categoryModel from '../models/Category.js';
 import { HttpError } from '../utils/error.js';
+import prisma from '../configs/dbConfig.js';
 
 export const getAllCategories = async (req, res, next) => {
   try {
@@ -27,7 +28,7 @@ export const getCategoryById = async (req, res, next) => {
       throw new HttpError('ID kategori tidak valid', 400);
     }
 
-    const category = await categoryModel.getCategoryById(id);
+    const category = await categoryModel.getCategoryById(parseInt(id));
 
     if (!category) {
       throw new HttpError('Kategori tidak ditemukan', 404);
@@ -45,6 +46,20 @@ export const getCategoryById = async (req, res, next) => {
 export const createCategory = async (req, res, next) => {
   try {
     const { validatedCategoryData } = req;
+
+    // Cek apakah kategori dengan nama yang sama sudah ada
+    const existingCategory = await prisma.category.findFirst({
+      where: {
+        name: {
+          equals: validatedCategoryData.name,
+          mode: 'insensitive',
+        },
+      },
+    });
+
+    if (existingCategory) {
+      throw new HttpError('Kategori dengan nama tersebut sudah ada', 400);
+    }
 
     const category = await categoryModel.createCategory(validatedCategoryData);
 
@@ -69,14 +84,34 @@ export const updateCategory = async (req, res, next) => {
       throw new HttpError('ID kategori tidak valid', 400);
     }
 
-    const existingCategory = await categoryModel.getCategoryById(id);
+    const parsedId = parseInt(id);
+
+    // Cek apakah kategori ada
+    const existingCategory = await categoryModel.getCategoryById(parsedId);
 
     if (!existingCategory) {
       throw new HttpError('Kategori tidak ditemukan', 404);
     }
 
+    // Cek apakah nama kategori sudah digunakan oleh kategori lain
+    const duplicateCategory = await prisma.category.findFirst({
+      where: {
+        name: {
+          equals: validatedCategoryData.name,
+          mode: 'insensitive',
+        },
+        id: {
+          not: parsedId,
+        },
+      },
+    });
+
+    if (duplicateCategory) {
+      throw new HttpError('Kategori dengan nama tersebut sudah ada', 400);
+    }
+
     const category = await categoryModel.updateCategory(
-      parseInt(id),
+      parsedId,
       validatedCategoryData,
     );
 
@@ -100,13 +135,34 @@ export const deleteCategory = async (req, res, next) => {
       throw new HttpError('ID kategori tidak valid', 400);
     }
 
-    const existingCategory = await categoryModel.getCategoryById(id);
+    const parsedId = parseInt(id);
+
+    // Cek apakah kategori ada
+    const existingCategory = await categoryModel.getCategoryById(parsedId);
 
     if (!existingCategory) {
       throw new HttpError('Kategori tidak ditemukan', 404);
     }
 
-    await categoryModel.deleteCategory(parseInt(id));
+    // Cek apakah kategori digunakan oleh event
+    const eventCount = await prisma.event.count({
+      where: {
+        categories: {
+          some: {
+            id: parsedId,
+          },
+        },
+      },
+    });
+
+    if (eventCount > 0) {
+      throw new HttpError(
+        'Kategori ini sedang digunakan oleh event, tidak dapat dihapus',
+        400,
+      );
+    }
+
+    await categoryModel.deleteCategory(parsedId);
 
     res.status(200).json({
       message: 'Kategori berhasil dihapus',
