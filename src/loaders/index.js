ecddd5df9f0cc606abca7e1c84fc2ed72bb1700d
nazladiohevin <nazlahevinbusiness@gmail.com>
2025-05-07 07:23:33 +0700

    feat(ui_email): implement ui email verif password & forgot password

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,8 +2,10 @@ import cors from './cors.js';
 import helmet from './helmet.js';
 import morgan from './morgan.js';
 import common from './common.js';
+import ejs from "./ejs.js";
 
 export default (app) => {
+  ejs(app);
   morgan(app);
   cors(app);
   helmet(app);
